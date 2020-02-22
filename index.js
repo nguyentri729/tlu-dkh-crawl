0bc0dfea900e2fefc3a7a2f43c4c0798e5b5abb5
Nguyen Tri <nguyentri729.2018@gmail.com>
2020-02-22 19:34:42 +0700

    create request function va di an com :)))

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('say hello Viet Nam...')
+require("dotenv").config();
+const rp = require("request-promise");
+const cheerio = require("cheerio");
+const URL = "http://dkh.tlu.edu.vn/CMCSoft.IU.Web.info";
+var $;
+//create request 
+const request = async (endpoint, option) => {
+  let options = {
+    uri: URL + endpoint,
+    transform: body => {
+      return cheerio.load(body);
+    },
+    ...option
+  };
+  await rp(options)
+    .then(result => {
+      $ = result;
+    })
+    .catch(err => {
+      throw err;
+    });
+};
+
